0a6b2c2e20e0e6ae48bb223f305eb990b75930c6
Anton Wilhelm <timaschew@gmail.com>
2015-01-06 17:25:17 +0100

    add mega crazy shit form validation and live calculation, handle each room in fuchs independently

--- src/files/js/buchen.js
+++ src/files/js/buchen.js
@@ -4,6 +4,7 @@ var fields = 'description,items(id,created,end,start,status,summary,updated,loca
 var base = 'https://www.googleapis.com/calendar/v3/calendars/';
 var url = base + calendarId + '/events?fields=' + fields + '&key=' + apiKey;
 
+var CLEAN_BASE = 30;
 var DE_FORMATTER = 'DD.MM.YYYY';
 var DATEPICKER_SELECTOR = '.conainter.booking .input-daterange';
 var DATEPICKER_OPTS = {
@@ -50,7 +51,9 @@ var calcCalendar = function(dayInMonth) {
     var cellTemplate = "<div class='calDate'></div>"+
     "<div class='flat SL'><span class='title'></span></div>"+
     "<div class='flat EH'><span class='title'></span></div>"+
-    "<div class='flat FS'><span class='title'></span></div>";
+    "<div class='flat F1'><span class='title'></span></div>"+
+    "<div class='flat F2'><span class='title'></span></div>"+
+    "<div class='flat F3'><span class='title'></span></div>";
     while (start.isBefore(end)) {
         var dateOfMonth = start.date();
         weekDay = start.format('e');
@@ -103,6 +106,15 @@ var updateItems = function(data) {
                 case 'Fuchs': 
                     item.location = 'FS';
                     break;
+                case 'Fuchs1', 'FS1':
+                    item.location = "F1";
+                    break;
+                case 'Fuchs2', 'FS2':
+                    item.location = "F2";
+                    break;
+                case 'Fuchs3', 'FS3':
+                    item.location = "F3";
+                    break;
             }
         }
 
@@ -120,20 +132,30 @@ var updateItems = function(data) {
                 } else {
                     cssClass = 'middle'
                 }
-                var flat = $('.date'+date+' .'+item.location);
-                flat.addClass(cssClass);
-                //data-toggle="tooltip", data-placement="top", title="
-                flat.attr('data-toggle', 'tooltip');
-                flat.attr('data-placement', 'top');
-                flat.attr('title', item.summary);
-                //flat.find('.title').text(item.summary);
-                flat.append('<div data-id="'+item.id+'"></div>');
+                if (item.location === 'FS') {
+                    setFlat($('.date'+date+' .F1'), item, cssClass);
+                    setFlat($('.date'+date+' .F2'), item, cssClass);
+                    setFlat($('.date'+date+' .F3'), item, cssClass);
+                } else {
+                    setFlat($('.date'+date+' .'+item.location), item, cssClass);
+                }
+                
             }
             tmp.add(1, 'days');
         }
     });
     // init the flat tooltips
     $('[data-toggle="tooltip"]').tooltip();
+};
+
+var setFlat = function(flat, item, cssClass) {
+    flat.addClass(cssClass);
+    //data-toggle="tooltip", data-placement="top", title="
+    flat.attr('data-toggle', 'tooltip');
+    flat.attr('data-placement', 'top');
+    flat.attr('title', item.summary);
+    //flat.find('.title').text(item.summary);
+    flat.append('<div data-id="'+item.id+'"></div>');
 };
 
 var sortItems = function(data) {
@@ -163,6 +185,207 @@ var updateItemsForCurrentMonth = function() {
     }
 }; 
 
+
+var calcNights = function() {
+    var from = $('#b_arrival').val() || '-';
+    var to = $('#b_departure').val() || '-';
+    var fromDate = moment(from, DE_FORMATTER);
+    var toDate = moment(to, DE_FORMATTER);
+    var nights = moment(toDate).diff(fromDate, 'days');
+    return nights;
+};
+
+var calcTotalGuests = function() {
+    var inputs = $('#guests_control input');
+    var a = parseInt(inputs[0].value || 0);
+    var b = parseInt(inputs[1].value || 0);
+    var c = parseInt(inputs[2].value || 0);
+    var d = parseInt(inputs[3].value || 0);
+    var totalGuests = a+b+c+d;
+    
+    return totalGuests;
+}
+
+var calculatePrice = function() {
+    // sum up total guests
+    var totalGuests = calcTotalGuests()
+    $('#b_guests_total').val(totalGuests);
+    // validte flat size and show warning if needed
+    if (totalGuests > parseInt($('#flat_size').val())) {
+        $('.guests-size').addClass('has-warning');
+        $('label.size-overflow').show();
+    } else {
+        $('.guests-size').removeClass('has-warning');
+        $('label.size-overflow').hide();
+    }
+
+    if ($("#arrival").val() === '' || $("#b_departure").val() === '') {
+        resetCalculation();
+        return;
+    }
+    if (totalGuests <= 0) {
+        resetCalculation();
+        return;
+    }
+
+
+    // flat and nights = base price
+    var select = $('#b_flat');
+    var base = parseInt(select.find("[value="+select.val()+"]").attr('data-price'));
+    var nights = calcNights();
+
+    var extraTreshold = 0;
+
+    // sepcial logic for fuchs rooms
+    if (select.val() === 'Fuchs') {
+        var values = getDetailsForFuchs();
+        var result = values[0] + values[1] + values[2];
+        
+        if (result === 0) {
+            resetCalculation();
+            return;
+        } 
+        extraTreshold = result;
+        base = base * result;
+
+    } else {
+        // Schmetterling and Eichhoernchen
+        extraTreshold = 2;
+    }
+
+    $('#price-base').text(nights + ' Nächte * ' + base + ' €');
+
+    var toSubtract = extraTreshold;
+    
+    
+    
+    var extraSum = 0;
+    var extraText = [];
+    var fee = 0;
+
+    var map = {
+        adult: {
+            size: parseInt($('#b_guests_adult').val()),
+            price: parseInt($('#b_guests_adult').attr('data-price')),
+            fee: 2
+        },
+        teens: {
+            size: parseInt($('#b_guests_teens').val()),
+            price: parseInt($('#b_guests_teens').attr('data-price')),
+            fee: 1
+        },
+        children: {
+            size: parseInt($('#b_guests_children').val()),
+            price: parseInt($('#b_guests_children').attr('data-price')),
+            fee: 0
+        },
+        babies: {
+            size: parseInt($('#b_guests_children_free').val()),
+            price: parseInt($('#b_guests_children_free').attr('data-price')),
+            fee: 0
+        }
+    };
+    var array = ['adult', 'teens', 'children', 'babies'];
+
+    // calc fee
+    for (var i=0; i<array.length; i++) {
+        var tmp = map[array[i]];
+        if (tmp.size > 0) {
+            fee += tmp.size * tmp.fee;
+        }
+    }
+
+    // calc extra persons
+    if (totalGuests > extraTreshold) {
+        // compensate the person inclusive amount
+        for (var i=0; i<array.length; i++) {
+            var tmp = map[array[i]];
+            if (tmp.size > 0) {
+                while (tmp.size > 0 && toSubtract > 0) {
+                    tmp.size--;
+                    toSubtract--;
+                }
+            }
+        }
+        // the add now the extra costs
+        for (var i=0; i<array.length; i++) {
+            var tmp = map[array[i]];
+            if (tmp.size > 0 && tmp.price > 0) {
+                extraText.push(tmp.size + ' * ' + tmp.price + ' €');
+                extraSum += tmp.size * tmp.price;
+            }
+        }
+        
+        // special offer 8 persons for fuchs = 120 €
+        if (totalGuests > 7 && extraSum > (120 - base)) {
+            extraSum -= map.adult.price;
+            extraText.push('Aktions Rabatt');
+        }
+
+        $('#price-extra').text(nights + ' Nächte * (' + extraText.join(' + ')+')');
+    } else {
+        $('#price-extra').text('–');
+    }
+
+    // clean
+    var extraClean = 0;
+    if (totalGuests > 4) {
+        extraClean = (totalGuests - 4) * 5;
+    }
+    var totalClean = CLEAN_BASE + extraClean;
+    $('#price-clean').text(totalClean + ' €');
+
+    // total price
+    $('#price-sum').text((base*nights + extraSum*nights + totalClean) + ' €');
+
+    // fee
+    $('#price-fee').text(fee*nights + ' €');
+};
+
+var resetCalculation = function() {
+    $('#price-base').text('');
+    $('#price-extra').text('');
+    $('#price-clean').text('');
+    $('#price-sum').text('');
+    $('#price-fee').text('');
+};
+
+var getDetailsForFuchs = function() {
+    var inputs = $('#flat_fuchs_detail input');
+    var values = inputs.map(function(i, e) {return e.checked === false ? 0 : 1});
+    return values;
+};
+
+var getValidationLabel = function(referenceElement) {
+    if ($(referenceElement).prev().is('label.validation')) {
+        return $(referenceElement).prev()[0];
+    }
+    return $(referenceElement).parents('div.form-group').find('[for='+referenceElement.id+']')[0];
+};
+
+var extraValidation = function() {
+    // check that at least one person is booked
+    if (calcTotalGuests() <= 0) {
+        $('.guests-size').addClass('has-error');
+        $('label.size-underflow').show();
+        return false;
+    } else {
+        $('.guests-size').removeClass('has-error');
+        $('label.size-underflow').hide();
+    }
+
+    // check if at least one room for fuchs was checked
+    var flatDetails = getDetailsForFuchs();
+    var flat = $('#b_flat').val();
+    if (flat === 'Fuchs' && flatDetails[0] + flatDetails[1] + flatDetails[1] <= 0) {
+        $('#flat_fuchs_detail').addClass('has-error');
+        return false;
+    } else {
+        $('#flat_fuchs_detail').removeClass('has-error');
+    }
+
+    return true;
+};
 
 $(document).ready(function() {
 
@@ -197,8 +420,45 @@ $(document).ready(function() {
     $('.filter-EH').change(function() {
         $('.EH').toggle();
     });
-    $('.filter-FS').change(function() {
-        $('.FS').toggle();
+    $('.filter-F1').change(function() {
+        $('.F1').toggle();
+    });
+    $('.filter-F2').change(function() {
+        $('.F2').toggle();
+    });
+    $('.filter-F3').change(function() {
+        $('.F3').toggle();
+    });
+
+    $('#b_flat').on('change', function(e) {
+        if (e.target.value === 'Fuchs') {
+            $('#flat_fuchs_detail').show();
+            $('#flat_size').val("");
+        } else {
+            $('#flat_fuchs_detail').hide();
+            var flatSize = $(e.target).find("[value="+e.target.value+"]").attr('data-max');
+            $('#flat_size').val(flatSize);
+        }
+        calculatePrice();
+    });
+
+    $('#flat_fuchs_detail input').on('change', function(e) {
+        var values = getDetailsForFuchs();
+        var result = values[0] + values[1] + values[2];
+
+        // set max room
+        var flatSize = 0;
+        if (values[0]) flatSize += 4; // 4 beds
+        if (values[1]) flatSize += 2; // 1 double bed
+        if (values[2]) flatSize += 2; // 1 double bed
+        if (result === 3) flatSize += 1; // 1 couch
+        $('#flat_size').val(flatSize);
+
+        calculatePrice();
+    });
+
+    $('#guests_control input, #b_arrival, #b_departure').on('change', function(e) {
+        calculatePrice();
     });
 
     // blur immediately to avoid ios android keyboard overlay
@@ -206,44 +466,39 @@ $(document).ready(function() {
         e.target.blur();
     });
 
-    $('#guests_control input').on('change', function(e) {
-        var inputs = $('#guests_control input');
-        var a = parseInt(inputs[0].value || 0);
-        var b = parseInt(inputs[1].value || 0);
-        var c = parseInt(inputs[2].value || 0);
-        $('#b_guests_total').val(a+b+c);
-    });
-
     $('#b_submit').on('click', function(e) {
         e.preventDefault();
+
+        // remove validation errors if there were some
+        $('div.form-group.has-error').removeClass('has-error');
+
+        extraValidation();
 
         var form = $('.conainter.booking')[0];
         if (form.checkValidity && !form.checkValidity()) {
             var inputs = form.querySelectorAll("input");
             for (var i=0; i<inputs.length; i++) {
-                if (!inputs[i].validity.valid) {
-                    var msg = inputs[i].validationMessage;
-                    var popoverContainer = $(inputs[i]);
-                    popoverContainer.attr('data-placement', 'top');
-                    popoverContainer.attr('data-content', msg);
-                    popoverContainer.attr('data-trigger', 'manual focus');
-                    popoverContainer.popover('show');
-                    popoverContainer.on('hidden.bs.popover', function (e) {
-                      $(e.target).popover('destroy');
-                    });
+                var tmp = inputs[i];
+                if (!tmp.validity.valid) {
+                    var msg = tmp.validationMessage;
+                    var validation = $(getValidationLabel(tmp));
+                    validation.text(msg);
+                    //label.show();
+                    $(tmp).parents('div.form-group').addClass('has-error');
+                    
                 }
             }
+            
         } else {
+            if (!extraValidation()) return;
 
             var flat = $('#b_flat').val() || '-';
             var from = $('#b_arrival').val() || '-';
             var to = $('#b_departure').val() || '-';
-            var fromDate = moment(from, DE_FORMATTER);
-            var toDate = moment(to, DE_FORMATTER);
-            var nights = moment(toDate).diff(fromDate, 'days');
-
+            var nights = calcNights();
             var guests = $('#b_guests_total').val() || '-';
-            var guests_adult = $('#b_guests_children').val() || '-';
+            var guests_adult = $('#b_guests_adult').val() || '-';
+            var guests_teens = $('#b_guests_teens').val() || '-';
             var guests_children = $('#b_guests_children').val() || '-';
             var guests_children_free = $('#b_guests_children_free').val() || '-';
             var name = $('#b_name').val() || '-';
@@ -252,10 +507,22 @@ $(document).ready(function() {
             var note = $('#b_note').val() || '-';
 
             var NL = "%0D%0A";
-            var subject = "Buchung: "+from+" - "+to+" / "+nights+" Nächte / "+guests+" Personen";
-            var body = "Erwachsene: "+guests_adult + NL +
-                "Kinder (bis 17): "+guests_children + NL + 
-                "Kinder (bis 9): "+guests_children_free + NL + 
+            var subject = "Buchung: "+from+" - "+to+" / "+nights+" Nächte in " + flat + " / "+guests+" Personen";
+            var flatDetails = getDetailsForFuchs();
+            var flatDetailsText = '';
+            if (flatDetails[0] + flatDetails[1] + flatDetails[1] > 0) {
+                var tmp = '';
+                tmp +=  flatDetails[0] ? ' 1. ' : '';
+                tmp +=  flatDetails[1] ? ' 2. ' : '';
+                tmp +=  flatDetails[2] ? ' 3. ' : '';
+                flatDetailsText = "Fuchs Zimmer:"+tmp + NL;
+            }
+
+            var body = flatDetailsText + 
+                "Erwachsene: "+guests_adult + NL +
+                "Kinder (bis 17): "+guests_teens + NL + 
+                "Kinder (bis 9): "+guests_children + NL +
+                "Kinder (bis 4): "+guests_children_free + NL + 
                 "Name: "+name + NL + 
                 "E-Mail: "+email + NL +
                 "Telefon: "+phone + NL +
@@ -265,6 +532,8 @@ $(document).ready(function() {
             window.location = "mailto:buchen@salztraeume-am-see.de?subject="+subject+"&body="+body;
         }
     });
+
+
 
     // enable tooltips booking
     $('[data-toggle="tooltip"]').tooltip();
