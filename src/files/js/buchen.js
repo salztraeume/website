19c9dab9167e9267f784638984e139f39ded18bf
Anton Wilhelm <timaschew@gmail.com>
2015-05-06 08:29:16 +0200

    fix prod url

--- src/files/js/buchen.js
+++ src/files/js/buchen.js
@@ -1,4 +1,4 @@
-var FORM_MAILER_URL = 'http://localhost:9771/submit';
+var FORM_MAILER_URL = 'http://thunderwave.de:9771/submit';
 var CLEAN_BASE = 30;
 var DE_FORMATTER = 'DD.MM.YYYY';
 var EN_FORMATTER = 'YYYY-MM-DD';
